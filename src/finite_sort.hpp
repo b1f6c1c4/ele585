b48a192c883471c37b85606ff1d115f774d07244
b1f6c1c4 <b1f6c1c4@gmail.com>
2019-04-22 15:30:36 -0400

    impl sorting network

--- src/finite_sort.hpp
+++ src/finite_sort.hpp
@@ -1,42 +1,87 @@
 #include <utility>
 
-template <typename T>
-inline void sort_unit(T &l, T& r)
+template <typename T, size_t NTotal, bool Debug = false>
+class finite_sort
 {
-    // auto lv = (reinterpret_cast<char *>(&l) - ptr) / sizeof(T);
-    // auto rv = (reinterpret_cast<char *>(&r) - ptr) / sizeof(T);
-    std::cerr << "[" << lv << "]--[" << rv << "]" << std::endl;
+private:
+    inline void sort_unit(T &l, T& r)
+    {
+        if (Debug)
+        {
+            auto lv = &l - _base;
+            auto rv = &r - _base;
+            for (size_t i = 0; i < lv; i++)
+                std::cerr << " | ";
+            std::cerr << " X-";
+            for (size_t i = lv + 1; i < rv; i++)
+                std::cerr << "-+-";
+            std::cerr << "-X ";
+            for (size_t i = rv + 1; i < NTotal; i++)
+                std::cerr << " | ";
+            std::cerr << "[" << lv << "]--[" << rv << "]" << std::endl;
+            _count++;
+        }
 
-    if (l > r)
-        std::swap(l, r);
-}
+        if (l > r)
+            std::swap(l, r);
+    }
 
-template <typename T, size_t N, size_t NS>
-inline typename std::enable_if<(N < 2), void>::type
-sort_scatter(T *d) { }
+    template <size_t N, size_t NS, ptrdiff_t B>
+        inline typename std::enable_if<(B < 0 || N * NS - NS + 1 <= B), void>::type
+        sort_scatter(T *d) { }
 
-template <typename T, size_t N, size_t NS>
-inline typename std::enable_if<(N == 2), void>::type
-sort_scatter(T *d)
-{
-    sort_unit<T>(d[0], d[NS]);
-}
+    template <size_t N, size_t NS>
+        void sort_scatter(T *d)
+        {
+            // std::cerr << "sort_scatter<" << N << ", " << NS << ">(d[" << d - _base << "])" << std::endl;
+            if (N < 2)
+                return;
+            if (N == 2)
+            {
+                sort_unit(d[0], d[NS]);
+                return;
+            }
+            constexpr auto Q = N / 2;
+            constexpr auto P = N - Q;
+            sort_scatter<P, NS>(d);
+            sort_scatter<Q, NS>(d + NS * P);
+            sort_scatter<P, NS * 2, P>(d);
+            sort_scatter<Q, NS * 2, P - NS>(d + NS);
+            for (size_t i = NS; i + NS < NS * N; i += NS * 2)
+                sort_unit(d[i], d[i + NS]);
+        }
 
-template <typename T, size_t N, size_t NS>
-inline typename std::enable_if<(N > 2), void>::type
-sort_scatter(T *d)
-{
-    sort_scatter<T, N - (N / 2), NS>(d);
-    sort_scatter<T, N / 2, NS>(d + NS * (N - (N / 2)));
-    sort_scatter<T, N - (N / 2), NS * 2>(d);
-    sort_scatter<T, N / 2, NS * 2>(d + NS);
-    for (size_t i = NS; i + NS < NS * N; i += NS * 2)
-        sort_unit<T>(d[i], d[i + NS]);
-}
+    template <size_t N, size_t NS, ptrdiff_t B>
+        inline typename std::enable_if<!(B < 0 || N * NS - NS + 1 <= B), void>::type
+        sort_scatter(T *d)
+        {
+            if (N < 2)
+                return;
+            // std::cerr << "sort_scatter<" << N << ", " << NS << ", " << B << ">(d[" << d - _base << "])" << std::endl;
+            if (N == 2)
+            {
+                sort_unit(d[0], d[NS]);
+                return;
+            }
+            constexpr auto Q = N / 2;
+            constexpr auto P = N - Q;
+            sort_scatter<P, NS, B>(d);
+            sort_scatter<Q, NS, B>(d + NS * P);
+            sort_scatter<P, NS * 2, P>(d);
+            sort_scatter<Q, NS * 2, P - NS>(d + NS);
+            for (size_t i = NS; i + NS < NS * N; i += NS * 2)
+                sort_unit(d[i], d[i + NS]);
+        }
 
-template <typename T, size_t N>
-inline void finite_sort(T *d)
-{
-    // ptr = reinterpret_cast<char *>(d);
-    sort_scatter<T, N, 1>(d);
-}
+    const T *_base;
+    size_t _count;
+public:
+    inline void sort(T *d)
+    {
+        if (Debug)
+            _base = d;
+        sort_scatter<NTotal, 1>(d);
+        if (Debug)
+            std::cerr << "Total = " << _count << std::endl;
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,15 @@
 
 int main(int argc, char *argv[])
 {
-    constexpr auto len = 5; // 1 * 1024 * 1024;
+    constexpr auto len = 1024 * 1024 * 1024;
+    constexpr auto grp = 8;
     auto buffer = new uint64_t[len];
 
     std::cin.read(reinterpret_cast<char *>(buffer), sizeof(*buffer) * len);
 
-    finite_sort<uint64_t, len>(buffer);
+    for (auto ptr = buffer; ptr < buffer + len; ptr += grp)
+        std::sort(ptr, ptr + grp);
+        // finite_sort<uint64_t, grp>().sort(ptr);
 
     std::cout.write(reinterpret_cast<const char *>(buffer), sizeof(*buffer) * len);
 
